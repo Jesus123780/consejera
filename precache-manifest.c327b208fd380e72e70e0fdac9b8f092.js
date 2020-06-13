4eff8f227d6d7fb48921959ec2656eb48771990a
Jesus123780 <53837840+Jesus123780@users.noreply.github.com>
2020-06-12 23:03:44 -0500

    Updates

---
+++ precache-manifest.c327b208fd380e72e70e0fdac9b8f092.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98939bc35a10c212b18f8f0f909761a3",
+    "url": "/consejera/index.html"
+  },
+  {
+    "revision": "800ce373893b543df00a",
+    "url": "/consejera/static/css/main.968aa5fd.chunk.css"
+  },
+  {
+    "revision": "be8764e90a80b8a08290",
+    "url": "/consejera/static/js/2.bf44d81a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/consejera/static/js/2.bf44d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800ce373893b543df00a",
+    "url": "/consejera/static/js/main.ee0811da.chunk.js"
+  },
+  {
+    "revision": "50ac44309da24e2fc002",
+    "url": "/consejera/static/js/runtime-main.ca9df7c9.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/consejera/static/media/city.91c109fc.jpg"
+  }
+]);
